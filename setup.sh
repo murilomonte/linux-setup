cec718512e642ec9d9c162ac872c735a7a084a96
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2023-02-06 15:30:44 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,10 +38,10 @@ if [[ $os = 'ubuntu' ]]; then
     echo "#===================#"
     sudo apt install flatpak -y
     sudo apt install plasma-discover-backend-flatpak -y
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
 
     # instalando firefox flatpak
-    flatpak install -y flathub org.mozilla.firefox
+    flatpak install flathub org.mozilla.firefox -y
 
     echo "#===================#"
     echo "Instalando icones"
@@ -51,7 +51,7 @@ if [[ $os = 'ubuntu' ]]; then
     echo "#===================#"
     echo "Instalando outros apps"
     echo "#===================#"
-    sudo apt install neofetch intel-gpu-tools htop scrcpy obs-studio
+    sudo apt install neofetch intel-gpu-tools htop scrcpy obs-studio -y
 
 elif [[ $os = 'fedora' ]]; then
     echo "#===================#"
@@ -62,36 +62,41 @@ elif [[ $os = 'fedora' ]]; then
     echo "#===================#"
     echo "Configurando rpm fusion"
     echo "#===================#"
-    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
     echo "#===================#"
     echo "Configurando video decode vaapi"
     echo "#===================#"
-    sudo dnf install ffmpeg libva-utils intel-media-driver
+    sudo dnf install ffmpeg libva-utils intel-media-driver -y
 
     echo "#===================#"
     echo "Adicionando suporte a flatpak e instalando apps"
     echo "#===================#"
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
 
     echo "#===================#"
     echo "Instalando outros apps"
     echo "#===================#"
-    sudo dnf install neofetch intel-gpu-tools htop gnome-tweak-tool scrcpy papirus-icon-theme -y
+    sudo dnf install neofetch intel-gpu-tools htop gnome-tweak-tool papirus-icon-theme -y
+
+    echo "#===================#"
+    echo "Debloat :)"
+    echo "#===================#"
+    sudo dnf remove akregator kamoso mediawriter kmag kgpg qt5-qdbusviewer kcharselect kcolorchooser dragon kmines kmahjongg kpat kruler kmousetool kmouth kolourpaint konversation krdc kfind kaddressbook kmail kontact korganizer ktnef kf5-akonadi-* dnfdragora -y
 fi
 
 echo "#===================#"
 echo "Instalando apps flatpak"
 echo "#===================#"
 # firefox github-desktop telegram flatseal steam
-flatpak install -y flathub io.github.shiftey.Desktop org.telegram.desktop com.github.tchx84.Flatseal com.valvesoftware.Steam com.github.libresprite.LibreSprite
+flatpak install flathub org.telegram.desktop com.github.tchx84.Flatseal com.valvesoftware.Steam com.github.libresprite.LibreSprite -y
 
 echo "#===================#"
 echo "Configurando tema dos apps flatpak"
 echo "#===================#"
 # confirando temas 
-flatpak install org.gtk.Gtk3theme.Breeze
-sudo flatpak override --system --filesystem=xdg-config/gtk-3.0:ro --filesystem=xdg-config/gtkrc-2.0:ro --filesystem=xdg-config/gtk-4.0:ro --filesystem=xdg-config/gtkrc:ro --env "GTK_THEME=Breeze"
+flatpak install org.gtk.Gtk3theme.Breeze -y
+sudo flatpak override --system --filesystem=xdg-config/gtk-3.0:ro --filesystem=xdg-config/gtkrc-2.0:ro --filesystem=xdg-config/gtk-4.0:ro --filesystem=xdg-config/gtkrc:ro --env "GTK_THEME=Breeze" -y
 
 
 echo "#===================#"
