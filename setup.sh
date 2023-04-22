5b62ffd37eef242e5176cccd13eb0c1f650aa248
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2023-04-22 17:59:19 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -89,8 +89,17 @@ elif [[ $os = 'fedora' ]]; then
     echo "#===================#"
     echo "Instalando apps"
     echo "#===================#"
-    sudo dnf install neofetch intel-gpu-tools htop papirus-icon-theme steam kdenlive obs-studio telegram-desktop -y
+    # configurando repositório do github-desktop
+    sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+    sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
 
+	# configurando repositório do vscode
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    
+    sudo dnf check-update
+    sudo dnf install neofetch intel-gpu-tools htop papirus-icon-theme steam kdenlive obs-studio telegram-desktop github-desktop code -y
+	
     if [[ $variant = 'workstation' ]]; then
         echo ""
         echo "#===================#"
